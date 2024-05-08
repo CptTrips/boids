31e52539b2a9058f101c8a39348977df03fdd23b
Matt Flinders <sodaime@gmail.com>
2024-05-08 19:18:54 +0100

    Add UI controls

--- Boids/BoidsUI.cpp
+++ Boids/BoidsUI.cpp
@@ -1,9 +1,25 @@
 #include "BoidsUI.h"
 
+BoidsUI::BoidsUI(UIState state)
+    : state(state)
+{
+}
+
 void BoidsUI::draw()
 {
 
     ImGui::Begin("Boids");
 
+    ImGui::SliderFloat("Cohesion", &(state.cohesion), 0.0, 5.0);
+
+    ImGui::SliderFloat("Alignment", &(state.alignment), 0.0, 5.0);
+
+    ImGui::SliderFloat("Separation", &(state.separation), 0.0, 5.0);
+
     ImGui::End();
+}
+
+UIState BoidsUI::getState() const
+{
+    return state;
 }
--- Boids/BoidsUI.h
+++ Boids/BoidsUI.h
@@ -2,11 +2,23 @@
 
 #include "UI.h"
 
+struct UIState
+{
+
+    float cohesion, alignment, separation;
+};
+
 class BoidsUI : public UI
 {
 
+    UIState state;
+
 public:
 
+    BoidsUI(UIState state);
+
     virtual void draw();
+
+    UIState getState() const;
 };
 
